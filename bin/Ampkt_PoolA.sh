bb6b7203c049882f14dc46bc3510ec2a1222535b
Emmanueleon <56766014+Emmanueleon@users.noreply.github.com>
2020-05-26 12:02:21 -0500

    Update Ampkt_PoolA.sh

--- bin/Ampkt_PoolA.sh
+++ bin/Ampkt_PoolA.sh
@@ -1,11 +1,17 @@
+!/bin/bash
 
+#This script was made by Emmanuel Cervantes Monroy, it's objective is to demultiplex and filter sequences in order to create a .biom file 
 
+#This script use the program amptk (https://amptk.readthedocs.io/en/latest/)
+
+### Run amp through conda
 conda activate amptk
 
 ###Variable statement
+#This script assume that your working directory is bin directory
 raw_dir=../data/PoolA/
 out_cleaned=../output/ampkt_results/
-export usearch=~/Reference\ databases/usearch9
 
-###Pre processing Script (this it's only one of 24 different barcode sequence) 
+###Pre processing Script 
+#Objective: Assemble the Forward and Reverse primers, in addition to eliminate and filter from unnecessary primers and short sequences 
 amptk illumina -i $raw_dir -o $out_cleaned -f TGTGCC -r CGTGAT
